216d4e94e61c34f018b73dbdb45ad487b1cd1432
Adam J. Leuer <Adam@Adams-Mac-Pro.local>
2014-01-23 02:14:09 -0600

    Initial Commit

---
+++ Word Finder/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Word Finder
+//
+//  Created by Adam James Leuer on 1/23/14.
+//  Copyright (c) 2014 Adam James Leuer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
